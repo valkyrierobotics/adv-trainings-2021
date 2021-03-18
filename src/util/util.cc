2f3219e495e7e79fc5077d5ec7333e05ab3c8f41
Visisht Madhavan Sriraman <mailmaadhu@gmail.com>
2021-03-18 08:10:12 -0700

    Add initial Output and Sensors util

--- src/util/util.cc
+++ src/util/util.cc
@@ -52,20 +52,58 @@ std::optional<Joystick> decode_joystick(const std::array<unsigned char, 4> &raw)
 
 std::array<unsigned char, 4> encode_output(const Output &raw) {
   std::array<unsigned char, 4> ret;
+  
+  ret[3] = raw.dt_left_voltage;
+  ret[2] = raw.dt_right_voltage;
+  ret[1] = raw.arm_voltage;
+  ret[0] = 0;
+
+  ret[0] |= (joystick.enabled << 7); //Incorrect Logic
+  ret[0] |= (joystick.gripper_toggle << 2);
+  ret[0] |= (joystick.roller_fwd << 1);
+  ret[0] |= (joystick.roller_rev << 0);
   return ret;
+
 }
 
 std::optional<Output> decode_output(const std::array<unsigned char, 4> &raw) {
+  
+  if (raw[3] != 0xFE) {
+    return std::nullopt;
+  }
+
   Output ret;
+  ret.dt_left_voltage = raw[3];
+  ret.dt_right_voltage = raw[2];
+  ret.arm_voltage = raw[1];
+
+  ret.gripper_open = raw[0] & (1 << 2);
+  ret.roller_forward = raw[0] & (1 << 1);
+  ret.roller_backwards = raw[0] & (1 << 0);
+
   return std::optional(ret);
 }
 
 std::array<unsigned char, 4> encode_sensors(const Sensors &sensors) {
-  std::array<unsigned char, 4> ret{};
+std::array<unsigned char, 4> ret{};
+
+  ret[2] = sensors.arm_position;
+  ret[1] = sensors.lower_limit_on;
+  ret[0] = sensors.upper_limit_on;
+
   return ret;
 }
 
 std::optional<Sensors> decode_sensors(const std::array<unsigned char, 4> &raw) {
+
+  if (raw[3] != 0xFE) {
+    return std::nullopt;
+  }
+
   Sensors ret;
+  ret.arm_position = raw[2]; //to convert hex value back to float
+  ret.lower_limit_on = raw[1];
+  ret.upper_limit_on = raw[0];
+
   return std::optional(ret);
 }
